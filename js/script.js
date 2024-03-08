75b3db8c4fc88de71e74e3bb29ede058db2ebca7
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-08 15:42:13 +0100

    add classsName and setAttribute to js

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipl
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
  /* template 
- <div id="boxes d-flex justify-content-center">
+ <div id="boxes class="d-flex justify-content-center gap-2">
             <div class="box"></div>
         </div> 
  */
 
 // creo il contenitore nel dom usando createElemene() 
 const boxWrapper = document.createElement('div');
-console.log(boxWrapper);
+        // console.log(boxWrapper);
 
+//aggiungo gli attributi (className)
+boxWrapper.className = ('d-flex justify-content-center align-content-center gap-2');
+        // console.log(boxWrapper)
 
-
+// aggiungo l'id e il box al div
+boxWrapper.setAttribute ('id','boxes')
+        console.log(boxWrapper);
 
 // dichiaro la variabile per i numeri/parole
 let numBox = 100;
-console-console.log(numBox);
-//aggiungo la condizione con for per il ciclo
-// for(let i = (1 % 3 === 0 ) && (1 % 5 === 0) ; i <=numBox ; ((i = i + 3) && (i = i + 5)){
-// }
+//console-console.log(numBox);
 
-// );
+
+//aggiungo la condizione con for per il ciclo
+for(let i = (1 % 3 === 0 ) && (1 % 5 === 0) ; i <=numBox ; ((i = i + 3) && (i = i + 5)){
+
+
+});
