92b6a3e3d5256e9775de3791bd82013ebf9b9ffb
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-08 13:47:47 +0100

    add template in js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+ /* template 
+ <div id="boxes d-flex justify-content-center">
+            <div class="box"></div>
+        </div> 
+ */
+
+// creiamo il contenitore 
+
+
+// dichiaro la variabile per i numeri/parole
+let numBox = 100;
+console-console.log(numBox);
+//aggiungo la condizione con for per il ciclo
+for(let i = (1 % 3 === 0 ) && (1 % 5 === 0) ; i <=numBox ; ((i = i + 3) && (i = i + 5)){
+}
+
+);
