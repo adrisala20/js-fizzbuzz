8a8575c8af504b33147d11fec718d382005fc841
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-08 13:54:50 +0100

    add create.element in js

--- js/script.js
+++ js/script.js
@@ -9,14 +9,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
         </div> 
  */
 
-// creiamo il contenitore 
+// creo il contenitore nel dom usando createElemene() 
+const boxWrapper = document.createElement('div');
+console.log(boxWrapper);
+
+
 
 
 // dichiaro la variabile per i numeri/parole
 let numBox = 100;
 console-console.log(numBox);
 //aggiungo la condizione con for per il ciclo
-for(let i = (1 % 3 === 0 ) && (1 % 5 === 0) ; i <=numBox ; ((i = i + 3) && (i = i + 5)){
-}
+// for(let i = (1 % 3 === 0 ) && (1 % 5 === 0) ; i <=numBox ; ((i = i + 3) && (i = i + 5)){
+// }
 
-);
+// );
