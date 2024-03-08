7f110213491bd9b3a43f5807b78ec72e6f1f8c75
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-08 16:12:39 +0100

    add first condition %3 & %5

--- js/script.js
+++ js/script.js
@@ -23,11 +23,13 @@ boxWrapper.setAttribute ('id','boxes')
 
 // dichiaro la variabile per i numeri/parole
 let numBox = 100;
+let tempHtml='';
 //console-console.log(numBox);
 
 
-//aggiungo la condizione con for per il ciclo
-for(let i = (1 % 3 === 0 ) && (1 % 5 === 0) ; i <=numBox ; ((i = i + 3) && (i = i + 5)){
+/*     aggiungo la condizione con For per i numeri che sono sia multipli di 3 che di 5, stampi “FizzBuzz”   */
 
-
-});
+for(let i = 0 ; i <= numBox ; i++){
+    let number = (i % 3 === 0) && (i % 5 === 0)? 'fizzbuzz' : number;
+    console.log(number);
+}
